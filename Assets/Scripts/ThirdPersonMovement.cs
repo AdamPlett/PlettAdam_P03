6d297a4e4b9d5a6431d7a553e6f9e393919c25a9
Adam <Adamqplett@gmail.com>
2020-11-20 20:53:00 -0600

    Added Sprinting and barebones teleport that may need to be reworked

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -5,15 +5,36 @@ using UnityEngine;
 public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;
+    public GameObject player;
     public Transform cam;
 
     public float speed = 6f;
+    public float sprintSpeed = 10f;
+    public float dashDistance = 200f;
     public float turnSmoothTime = 0.1f;
+    bool isSprinting;
+    int sprintCount;
+    Vector3 moveDir;
 
     float turnSmoothVelocity;
+    private void Start()
+    {
+        isSprinting = false;
+        sprintCount = 0;
+    }
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            if (sprintCount % 2 == 0) isSprinting = true;
+            else isSprinting = false;
+            sprintCount++;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            teleport();
+        }
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
@@ -24,8 +45,13 @@ public class ThirdPersonMovement : MonoBehaviour
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            if (isSprinting == true) controller.Move(moveDir.normalized * sprintSpeed * Time.deltaTime);
+            else controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
+    }
+    void teleport()
+    {
+        controller.Move(moveDir.normalized * dashDistance * Time.deltaTime);
     }
 }
